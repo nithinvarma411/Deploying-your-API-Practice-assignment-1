13ce0035e10ad65f74d13482ba9d27ae0b87d8b6
nithinvarma <nithinvarma411@gmail.com>
2025-01-24 12:50:29 +0530

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const { resolve } = require('path');
+const dotenv = require('dotenv').config();
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
 
 const app = express();
 const port = 3010;
@@ -13,3 +19,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+console.log(config);
+module.exports = config;
